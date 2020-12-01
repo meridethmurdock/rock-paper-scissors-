b9d21fd13aa5de1905c0428b6a771d6ce18717cc
Merideth Murdock <meridethmurdock@Merideths-MacBook-Pro.local>
2020-11-30 19:52:30 -0600

    test

--- script.js
+++ script.js
@@ -14,41 +14,44 @@ function RPS() {
   var y = 0;
   
   for (i = 0; i < 6; i++) {
+
+    if (x == 3) {
+        alert("You won! I'll remeber this when the robots take over.")
+        break; 
+    } else if (y == 3) {
+        alert("You lost... Better luck next time!")
+        break;
+    } 
   
     var playerChoice = prompt("Rock, paper, scissors!");
     var playerChoice = playerChoice.toLowerCase();
-    const computerChoice = () => (Math.floor(Math.random() * 3));
+    var computerChoice = () => (Math.ceil(Math.random() * 3))
+    console.log(computerChoice());
 
     
-        if (x == 2) {
-            alert("You won! I'll remeber this when the robots take over.")
-            break; 
-        } else if (y == 3) {
-            alert("You lost... Better luck next time!")
-            break;
-        } else if (playerChoice == "rock" && computerChoice == "1") { 
+        if (playerChoice == "rock" && computerChoice() == 1) { 
             alert(`Rock does not affect to rock. You have ${x} point(s)!`);
-        } else if (playerChoice == "paper" && computerChoice == "1") {
+        } else if (playerChoice == "paper" && computerChoice() == 1) {
             x++;
             alert(`Paper beats rock. You have ${x} point(s)!`); 
-        } else if (playerChoice == "scissors" && computerChoice == "1") {
+        } else if (playerChoice == "scissors" && computerChoice() === 1) {
             y++;
             alert(`Scissors loses to rock. You have ${x} point(s)!`); 
-        } else if (playerChoice == "rock" && computerChoice == "2") {
+        } else if (playerChoice == "rock" && computerChoice() == 2) {
             y++;
             alert(`Rock is covered by paper, rendering it... useless. You have ${x} point(s)!`); 
-        } else if (playerChoice == "paper" && computerChoice == "2") {
+        } else if (playerChoice == "paper" && computerChoice() == 2) {
             alert(`Paper does not affect paper. You have ${x} point(s)!`); 
-        } else if (playerChoice == "scissors" && computerChoice == "2") {
+        } else if (playerChoice == "scissors" && computerChoice == 2) {
             x++;
             alert(`Scissors obliterates the paper! You have ${x} point(s)!`); 
-        } else if (playerChoice == "rock" && computerChoice == "3") {
+        } else if (playerChoice == "rock" && computerChoice() == 3) {
             x++;
             alert(`Rock smashes scissors to bits. You have ${x} point(s)!`);
-        } else if (playerChoice == "paper" && computerChoice == "3") {
+        } else if (playerChoice == "paper" && computerChoice() == 3) {
             y++;
             alert(`Paper is shredded by scissors... You have ${x} point(s)!`); 
-        } else if (playerChoice == "scissors" && computerChoice == "3") {
+        } else if (playerChoice == "scissors" && computerChoice() == 3) {
             alert(`Scissors does not affect scissors. You have ${x} point(s)!`); 
         } else {
           alert("didn't quite catch that...");
@@ -57,6 +60,8 @@ function RPS() {
     }
     
 }
+
+
 
 
 
